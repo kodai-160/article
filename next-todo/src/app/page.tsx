c79d1c6eedfaac3a60d13c6f0c531e7fe33083c7
kodai <kodaiadvance@icloud.com>
2025-02-24 14:10:08 +0900

    Next.jsのファイルの書き方の基本を学ぶ

--- next-todo/src/app/page.tsx
+++ next-todo/src/app/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image"
+import Link from "next/link"
+import Price from "./_components/Price";
+
 export default function Home() {
-  return <>home</>
+  return (
+    <>
+      Hello world
+      <Image src="/icon.png" alt="icon" width={100} height={100} />
+      <Link href="/cart">Go to cart</Link>
+      <Price />
+    </>
+  );
 }
